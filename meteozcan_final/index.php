fba0c9938c998d81de723281a72dd714149c8721
meteozcan06 <48053868+meteozcan06@users.noreply.github.com>
2021-01-12 22:44:59 +0300

    Add files via upload

---
+++ meteozcan_final/index.php
@@ -0,0 +1,60 @@
+<?php
+/* Bağlantı Kurulması */
+require_once("baglanti.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mete Özcan">
+    <meta name="generator" content="Jekyll v4.0.1">
+    <title>MY WebPage</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/cover/">
+
+<link href="bootstrap.min.css" rel="stylesheet">
+<link href="bs2.css" rel="stylesheet">
+  </head>
+  <body class="text-center">
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+
+  <main id="ks" role="main" class="inner cover">
+    <h1 class="cover-heading">Benim Kütüphanem</h1>
+    <p class="lead">Yemekler Hakkında</p>
+      <nav class="navbar navbar-expand-md navbar-dark">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item" >
+        <a class="nav-link" href="index.php">Ana Sayfa </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="kitap.php">Kitap İşlemleri</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="yazar.php">Yazar İşlemleri</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="yayinevi.php">Yayınevi İşlemleri</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+  </main>
+
+
+  <footer class="mastfoot mt-auto">
+    <div class="inner">
+        <p>This Page Was Edited By <a href="https://www.instagram.com/meteeozcan/?hl=tr ">Mete Özcan</a></p>
+    </div>
+  </footer>
+</div>
+    
+<script src="jquery-3.4.1.slim.min.js"></script>
+      <script src="bootstrap.bundle.min.js"></script>
+
+</body></html>
